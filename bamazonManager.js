307cb4f7a2e7ec126d181287df96a27cffe71422
Robert Prine <robertprine2@yahoo.com>
2016-06-17 21:37:08 -0400

    Added prompt for list of options and a switch statement to match the list of options

--- bamazonManager.js
+++ bamazonManager.js
@@ -11,13 +11,60 @@ var connection = mysql.createConnection({
 
 // List options: View Products for Sale, View Low Inventory, Add to Inventory, and Add New Product
 
-// switch for different options
+inquirer.prompt({
+	name: "choice",
+	type: "list",
+	message: "Would you like to:",
+	choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add a New Product"]
+}).then(function(answer) {
+
+	// switch for different options
+
+	switch(action) {
+		case 'View Products for Sale': 
+			productsForSale();
+			break;
+		case 'View Low Inventory':
+			lowInventory();
+			break;
+		case 'Add to Inventory':
+			addInventory();
+			break;
+		case 'Add a New Product':
+			newProduct();
+			break;
+	} // end of switch
+
+}); // end of inquirer prompt function
 
 // View Products for Sale: List products available for sale: the titemsIDs, names, prices, and quantities
 
+function productsForSale() {
+
+
+
+}; // end of productsForSale function
+
 // View Low Inventory: list all items for which the quantity available in stores is less than 5
+
+function lowInventory() {
+
+
+
+}; // end of lowInventory function
 
 // Add to Inventory: add more of any item currently in the store
 
+function addInventory() {
+
+
+
+}; // end of addInventory function
+
 // Add New Product: add a completely new product to the store
 
+function newProduct() {
+
+
+
+}; // end of newProduct function
